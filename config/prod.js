c1191a9d0acf6d0ee16b1efa25e484dd9035b1f3
Gareth <gareth.kh.lau@gmail.com>
2019-05-14 20:04:23 +0800

    Heroku env variables for config

---
+++ config/prod.js
@@ -0,0 +1,8 @@
+// keys for production
+module.exports = {
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    slackClientID: process.env.SLACK_CLIENT_ID,
+    slackClientSecret: process.env.SLACK_CLIENT_SECRET,
+	cookieKey: process.env.COOKIE_KEY
+};
