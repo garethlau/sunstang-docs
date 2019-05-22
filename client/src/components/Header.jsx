f4a983af5cdfeb8842c42c309a1ed5ab88103210
Gareth <gareth.kh.lau@gmail.com>
2019-05-22 23:26:19 +0800

    Header styles

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,13 +2,34 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {fetchUser} from '../actions';
 
-
+const headerStyles = {
+    height: "50px",
+    backgroundColor: "#373737",
+    color: "white",
+};
+const searchStyles = {
+    border: "2px solid black",
+    paddingLeft: "10px",
+    paddingRight: "10px",
+    paddingTop: "3px",
+    paddingBottom: "3px",
+    fontSize: "16px",
+}
 class Header extends Component {
     render() {
         return(
-            <>
-                Header
-            </>
+            <nav style={headerStyles}>
+                <ul style={{listStyleType: "none", margin: "0"}}>
+                    <li style={{float: "left", paddingTop: "13px"}}>LOGO</li>
+                    <li style={{float: "right", paddingTop: "13px", marginRight: "20px"}}>LOGO</li>
+                    <li style={{float: "right", paddingTop: "13px", marginRight: "20px"}}>LOGO</li>
+
+                    <li style={{float: "right", padding: "10px", marginRight: "20px"}}>
+                        <input type="text" placeholder="Search" style={searchStyles}></input>
+                    </li>
+
+                </ul>
+            </nav>
         )
     }
 }
