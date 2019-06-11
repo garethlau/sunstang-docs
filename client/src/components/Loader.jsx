7888efa37c688b59d3a27f8ac0d90bd6c81220a9
Gareth <gareth.kh.lau@gmail.com>
2019-06-11 22:18:42 +0800

    loading icon

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import Loader from 'react-loader-spinner';
 
-const Loader = (props) => {
+/*
+Really big pages, don't put anything. Default of 80 looks great
+Smaller loading areas, use a size of size={40};
+Really small areas, use size of size={20}
+*/
+
+const Loading = (props) => {
+	let width;
+	if (props.size === undefined) {
+		width = "80px"
+	}
+	else {
+		width = props.size + 'px'
+	}
 	return (
-		<div>
-			Loading
+		<div style={{margin: "auto", width: width, paddingTop: "125px"}}>
+			<Loader type="Triangle" color="rgb(255, 165, 0)" height={props.size || 80} width={props.size || 80} />
 		</div>
 	)
 };
-export default Loader;
+export default Loading;
