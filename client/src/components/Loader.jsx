ad8772fab1289ee876c782e3cc1ef10a221226e7
Gareth <gareth.kh.lau@gmail.com>
2019-06-12 22:31:12 +0800

    removed not used files

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import Loader from 'react-loader-spinner';
 
-import loaderStyles from '../styles/loaderStyles.module.css';
-
 /*
 Really big pages, don't put anything. Default of 80 looks great
 Smaller loading areas, use a size of size={40};
@@ -18,7 +16,7 @@ const Loading = (props) => {
 		width = props.size + 'px'
 	}
 	return (
-		<div class={loaderStyles.container} style={{width: width, margin: "auto", paddingTop: props.paddingTop || "150px"}}>
+		<div style={{width: width, margin: "auto", paddingTop: props.paddingTop || "150px"}}>
 			<Loader type="Triangle" color="rgb(255, 165, 0)" height={props.size || 80} width={props.size || 80} />
 		</div>
 	)
