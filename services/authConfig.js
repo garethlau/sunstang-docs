dd846c66d797fcb0e122b9d0ee32acf29a13c4e4
Gareth <gareth.kh.lau@gmail.com>
2019-06-21 22:33:02 +0800

    fixed auth config for prod

--- services/authConfig.js
+++ services/authConfig.js
@@ -1,13 +1,13 @@
-if (process.env.NODE_ENV) {
-    const devAuth = require('./devAuthConfig');
-    module.exports = {
-        adminEmails: devAuthConfig.adminEmails,
-        slackTeamId: devAuthConfig.slackTeamId
-    }
-}
-else {
+if (process.env.NODE_ENV === 'production') {
     module.exports = {
         adminEmails: process.env.ADMIN_EMAILS.split(','),
         slackTeamId: process.env.SLACK_TEAM_ID
+    }
+}
+else {
+    const devAuth = require('./devAuthConfig');
+    module.exports = {
+        adminEmails: devAuth.adminEmails,
+        slackTeamId: devAuth.slackTeamId
     }
 }
