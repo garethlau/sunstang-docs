f4dfabc49f56ef4360499f576ceb7cdd921d4fc3
Gareth <gareth.kh.lau@gmail.com>
2019-06-21 22:28:43 +0800

    fixed auth config for prod env

--- services/authConfig.js
+++ services/authConfig.js
@@ -1,13 +1,13 @@
-const devAuthConfig = require('./devAuthConfig');
-
-devAuthConfig ? 
+if (process.env.NODE_ENV) {
+    const devAuth = require('./devAuthConfig');
     module.exports = {
         adminEmails: devAuthConfig.adminEmails,
         slackTeamId: devAuthConfig.slackTeamId
-    } :
+    }
+}
+else {
     module.exports = {
         adminEmails: process.env.ADMIN_EMAILS.split(','),
         slackTeamId: process.env.SLACK_TEAM_ID
     }
-
-
+}
